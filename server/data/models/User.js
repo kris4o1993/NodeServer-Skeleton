f77a3483752e8f8d0ba8c6acb2d63da5caaf532f
Kristiyan Petrov <kris4o1993@gmail.com>
2014-10-11 15:43:47 +0300

    skeleton

---
+++ server/data/models/User.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+    encryption = require('../../utilities/encryption');
+
+module.exports.init = function() {
+    var userSchema = mongoose.Schema({
+        username: { type: String, require: '{PATH} is required', unique: true },
+        //firstName: { type: String, require: '{PATH} is required' },
+        //lastName: { type: String, require: '{PATH} is required' },
+        salt: String,
+        hashPass: String,
+        points: Number
+        //roles: [String]
+    });
+
+    userSchema.method({
+        authenticate: function(password) {
+            if (encryption.generateHashedPassword(this.salt, password) === this.hashPass) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    });
+
+    var User = mongoose.model('User', userSchema);
+};
